26a9ca092421a3089692f80c105549e03bffa72d
A.Ramazan Arslan <arslanrama84@gmail.com>
2023-06-05 14:51:24 +0200

    update the code

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -1,32 +1,31 @@
 <template>
     <div class="currency-converter">
-      <h1>Currency Converter</h1>
-      <div class="form-group">
-        <select v-model="from" id="from" class="form-control">
-          <option v-for="currency in currencies" :key="currency" :value="currency">
-            {{ currency }} {{ getFlagIcon(currency) }}
-          </option>
-        </select>
-        <div class="switch-icon" @click="switchCurrencies">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon">
-            <path d="M6 18a2 2 0 1 1 0-4h12a2 2 0 1 1 0 4H6zm0-6a2 2 0 1 1 0-4h12a2 2 0 1 1 0 4H6zm0-6a2 2 0 1 1 0-4h12a2 2 0 1 1 0 4H6z"/>
-          </svg>
-        </div>
-        <select v-model="to" id="to" class="form-control">
-          <option v-for="currency in currencies" :key="currency" :value="currency">
-            {{ currency }} {{ getFlagIcon(currency) }}
-          </option>
-        </select>
-      </div>
+      <h1 class="converter-title">Currency Converter</h1>
       <div class="form-group">
         <label for="amount">Amount:</label>
         <input v-model.number="amount" type="number" id="amount" class="form-control" />
+      </div>
+      <div class="form-group">
+        <select v-model="from" id="from" class="form-control">
+          <option v-for="currency in currencies" :key="currency" :value="currency">
+            <span class="flag-icon">{{ getFlagIcon(currency) }}</span> {{ currency }}
+          </option>
+        </select>
+        <div class="switch-icon" @click="switchCurrencies">
+            <i class="fa fa-exchange"></i>
+        </div>
+        <select v-model="to" id="to" class="form-control">
+          <option v-for="currency in currencies" :key="currency" :value="currency">
+            <span class="flag-icon">{{ getFlagIcon(currency) }}</span> {{ currency }}
+          </option>
+        </select>
       </div>
       <div class="result">
         <p>{{ amount }} {{ from }} <span class="arrow-icon">→</span> {{ convertedAmount }} {{ to }}</p>
       </div>
     </div>
   </template>
+  
   
   <script>
   import axios from "axios";
@@ -76,3 +75,4 @@
   </script>
   
   <style src="../styles/currency-converter.css" scoped></style>
+  
